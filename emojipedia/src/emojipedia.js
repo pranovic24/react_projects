ff7bbd28935c4825862afaed11416ad89b32a4d2
Pranay Sood <pranay.sd@gmail.com>
2022-10-22 18:01:47 +0800

    emojipedia and keeper-app-1

---
+++ emojipedia/src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🥹",
+    name: "Face Holding Back Tears",
+    meaning:
+      "Slightly smiling face with tears in the eyes. It can express gratitude, emotion or pride. At the same time, however, anger or rage could be the reason for barely being able to hold back the tears. The emotional interpretation depends on the context of the communication."
+  },
+  {
+    id: 5,
+    emoji: "🦾",
+    name: "Mechanical arm",
+    meaning:
+      "Be as strong as a robot! The high-tech arm prosthesis enables amputees to move their arms and hands. Expresses enthusiasm for Star Wars, robot technology, bionics or science fiction."
+  },
+  {
+    id: 6,
+    emoji: "🎾",
+    name: "Tennis",
+    meaning:
+      "Grand Slam tournament! The ball game is played by two or four players. Due to the light clothing of the players, tennis is also called the “white sport“. Formerly the sport of the rich, today a popular sport. In a figurative sense, the emoji can also stand for two sides of a certain topic someone is struggling with."
+  },
+
+];
+
+export default emojipedia;
